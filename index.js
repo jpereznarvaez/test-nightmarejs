666e262e30c6876bb805ddf92d85fee01b47784b
CristianDelRio <42577647+CristianJDelRio-Condor@users.noreply.github.com>
2019-01-31 11:58:08 -0500

    modified:   index.js

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
-var Nightmare = require("nightmare");
+var Nightmare = require('nightmare');
+const puppeteer = require('puppeteer');
 
-var fs = require("fs");
-var nightmare = Nightmare({
+(async () => {
+  console.time(`puppeteerTime`);
+
+  try {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1024,
+      height: 600
+    });
+
+    await page.goto('https://github.com');
+    await page.waitForSelector(
+      'body > div.application-main > div.py-6.py-sm-8.jumbotron-codelines > div > div > div.col-md-7.text-center.text-md-left > h1'
+    );
+
+    await page.screenshot({ path: './Puppeteer-github.png' });
+
+    console.timeEnd('puppeteerTime');
+
+    console.error('Puppeteer Error:', err);
+  } catch (e) {}
+})();
+
+(async () => {
+  console.time('nightmareTime');
+
+  var nightmare = Nightmare({
     maxWidth: 1204,
     maxHeight: 1800
-});
+  });
 
-var fileName = "./github.jpg";
-console.time("Time");
-return nightmare
+  await nightmare
     .viewport(1024, 600)
-    .goto(
-        "https://github.com"
+    .goto('https://github.com')
+    .wait(
+      'body > div.application-main > div.py-6.py-sm-8.jumbotron-codelines > div > div > div.col-md-7.text-center.text-md-left > h1'
     )
-    .wait(9000)
-    .screenshot(fileName)
+    .screenshot('./Nightmare-github.png')
     .end()
-    .then(function () {
-        console.log("Done!");
-        console.timeEnd("Time");
+    .then(function() {
+      console.log('Nightmare Done!');
+      console.timeEnd('nightmareTime');
     })
     .catch(err => {
-        console.error("Error:", err);
-        console.timeEnd("Time");
+      console.error('Nightmare Error:', err);
+      console.timeEnd('nightmareTime');
     });
+})();
